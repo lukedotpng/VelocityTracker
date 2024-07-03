b6ac90de17884f1c26e44f9ced27185f66563a8d
Rocco <rocco@master-chef.hitman>
2024-07-03 22:43:47 +0000

    Create VelocityTracker files

--- src/VelocityTracker.cpp
+++ src/VelocityTracker.cpp
@@ -1,4 +1,4 @@
-#include "MyMod.h"
+#include "VelocityTracker.h"
 
 #include <Logging.h>
 #include <IconsMaterialDesign.h>
@@ -7,48 +7,48 @@
 #include <Glacier/ZGameLoopManager.h>
 #include <Glacier/ZScene.h>
 
-void MyMod::OnEngineInitialized() {
-    Logger::Info("MyMod has been initialized!");
+void VelocityTracker::OnEngineInitialized() {
+    Logger::Info("VelocityTracker has been initialized!");
 
     // Register a function to be called on every game frame while the game is in play mode.
-    const ZMemberDelegate<MyMod, void(const SGameUpdateEvent&)> s_Delegate(this, &MyMod::OnFrameUpdate);
+    const ZMemberDelegate<VelocityTracker, void(const SGameUpdateEvent&)> s_Delegate(this, &VelocityTracker::OnFrameUpdate);
     Globals::GameLoopManager->RegisterFrameUpdate(s_Delegate, 1, EUpdateMode::eUpdatePlayMode);
 
     // Install a hook to print the name of the scene every time the game loads a new one.
-    Hooks::ZEntitySceneContext_LoadScene->AddDetour(this, &MyMod::OnLoadScene);
+    Hooks::ZEntitySceneContext_LoadScene->AddDetour(this, &VelocityTracker::OnLoadScene);
 }
 
-MyMod::~MyMod() {
+VelocityTracker::~VelocityTracker() {
     // Unregister our frame update function when the mod unloads.
-    const ZMemberDelegate<MyMod, void(const SGameUpdateEvent&)> s_Delegate(this, &MyMod::OnFrameUpdate);
+    const ZMemberDelegate<VelocityTracker, void(const SGameUpdateEvent&)> s_Delegate(this, &VelocityTracker::OnFrameUpdate);
     Globals::GameLoopManager->UnregisterFrameUpdate(s_Delegate, 1, EUpdateMode::eUpdatePlayMode);
 }
 
-void MyMod::OnDrawMenu() {
+void VelocityTracker::OnDrawMenu() {
     // Toggle our message when the user presses our button.
-    if (ImGui::Button(ICON_MD_LOCAL_FIRE_DEPARTMENT " MyMod")) {
+    if (ImGui::Button(ICON_MD_LOCAL_FIRE_DEPARTMENT " VelocityTracker")) {
         m_ShowMessage = !m_ShowMessage;
     }
 }
 
-void MyMod::OnDrawUI(bool p_HasFocus) {
+void VelocityTracker::OnDrawUI(bool p_HasFocus) {
     if (m_ShowMessage) {
         // Show a window for our mod.
-        if (ImGui::Begin("MyMod", &m_ShowMessage)) {
+        if (ImGui::Begin("VelocityTracker", &m_ShowMessage)) {
             // Only show these when the window is expanded.
-            ImGui::Text("Hello from MyMod!");
+            ImGui::Text("Hello from VelocityTracker!");
         }
         ImGui::End();
     }
 }
 
-void MyMod::OnFrameUpdate(const SGameUpdateEvent &p_UpdateEvent) {
+void VelocityTracker::OnFrameUpdate(const SGameUpdateEvent &p_UpdateEvent) {
     // This function is called every frame while the game is in play mode.
 }
 
-DEFINE_PLUGIN_DETOUR(MyMod, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData) {
+DEFINE_PLUGIN_DETOUR(VelocityTracker, void, OnLoadScene, ZEntitySceneContext* th, ZSceneData& p_SceneData) {
     Logger::Debug("Loading scene: {}", p_SceneData.m_sceneName);
     return HookResult<void>(HookAction::Continue());
 }
 
-DECLARE_ZHM_PLUGIN(MyMod);
+DECLARE_ZHM_PLUGIN(VelocityTracker);
